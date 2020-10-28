97b78718cb02d823c7bfb963d262884bf576475e
Karan Singh <31335553+karansinghtomar123@users.noreply.github.com>
2020-10-28 16:08:35 +0530

    Add files via upload

---
+++ Pro.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pro{  
+    public static void main(String args[]){
+    Scanner inp=new Scanner(System.in);
+    int rm=inp.nextInt();
+    if(rm==604){  
+    System.out.println("Java programming");  
+    }  
+    else if(rm==605){
+    System.out.println("Python programming");}
+    else
+    System.out.println("Invalid input");}
+    }
+ 
